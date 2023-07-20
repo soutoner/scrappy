ea1a92abe96948326975c5dcef906ce5785400a6
Adrian Gonzalez <adrigonle@gmail.com>
2023-07-20 22:31:27 +0200

    Little refactor

--- src/scrappers/bauhaus.ts
+++ src/scrappers/bauhaus.ts
@@ -1,11 +1,11 @@
-import { chromium, Page } from 'playwright';
+import { chromium } from 'playwright';
+import { Selectors } from './bauhaus/constants';
 import {
-  SELECTED_STORE_COOKIE,
-  Selectors,
-  StockResult,
-  StockTexts,
-} from './bauhaus/constants';
-import { buildProductPageUrl } from './bauhaus/helpers';
+  acceptConsent,
+  buildProductPageUrl,
+  setSelectedStore,
+  verifyStocks,
+} from './bauhaus/helpers';
 import { logger } from '../log';
 
 export async function scrape(productCode: string) {
@@ -19,54 +19,17 @@ export async function scrape(productCode: string) {
   await acceptConsent(page);
 
   let stockResult;
-  try {
-    const stockTexts = await page
-      .locator(Selectors.STOCK_ELEMENT)
-      .allInnerTexts();
+  const stockTexts = await page
+    .locator(Selectors.STOCK_ELEMENT)
+    .allInnerTexts();
 
+  if (stockTexts.length > 0) {
     stockResult = verifyStocks(stockTexts);
-  } catch (error: unknown) {
+  } else {
     stockResult = 'error';
   }
 
   logger.info(`Stock Result: ${stockResult}`);
 
   await browser.close();
-}
-
-function setSelectedStore(page: Page) {
-  page.context().addCookies([SELECTED_STORE_COOKIE]);
-}
-
-function acceptConsent(page: Page) {
-  return page.getByTestId(Selectors.ACCEPT_CONSENT).click();
-}
-
-function verifyStocks(stockTexts: string[]): StockResult {
-  const isAvailableOnline = verifyOnlineStock(stockTexts[0]);
-  const isAvailableInShop = verifyInShopStock(stockTexts[1]);
-
-  if (isAvailableOnline && isAvailableInShop) {
-    return 'both';
-  } else if (isAvailableOnline) {
-    return 'online';
-  } else if (isAvailableInShop) {
-    return 'in_shop';
-  } else {
-    return 'none';
-  }
-}
-
-function verifyOnlineStock(stockText: string) {
-  return (
-    !stockText?.includes(StockTexts.SOON) &&
-    stockText?.includes(StockTexts.AVAILABLE)
-  );
-}
-
-function verifyInShopStock(stockText: string) {
-  return (
-    !stockText?.includes(StockTexts.CURRENTLY) &&
-    stockText?.includes(StockTexts.AVAILABLE)
-  );
 }
--- src/scrappers/bauhaus/helpers.ts
+++ src/scrappers/bauhaus/helpers.ts
@@ -1,5 +1,49 @@
-import { BAUHAUS_URL } from './constants';
+import {
+  BAUHAUS_URL,
+  SELECTED_STORE_COOKIE,
+  Selectors,
+  StockResult,
+  StockTexts,
+} from './constants';
+import { Page } from 'playwright';
 
 export function buildProductPageUrl(productCode: string) {
   return `${BAUHAUS_URL}/p/${productCode}`;
+}
+
+export function setSelectedStore(page: Page) {
+  page.context().addCookies([SELECTED_STORE_COOKIE]);
+}
+
+export function acceptConsent(page: Page) {
+  return page.getByTestId(Selectors.ACCEPT_CONSENT).click();
+}
+
+export function verifyStocks(stockTexts: string[]): StockResult {
+  const isAvailableOnline = verifyOnlineStock(stockTexts[0]);
+  const isAvailableInShop = verifyInShopStock(stockTexts[1]);
+
+  if (isAvailableOnline && isAvailableInShop) {
+    return 'both';
+  } else if (isAvailableOnline) {
+    return 'online';
+  } else if (isAvailableInShop) {
+    return 'in_shop';
+  } else {
+    return 'none';
+  }
+}
+
+function verifyOnlineStock(stockText: string) {
+  return (
+    !stockText?.includes(StockTexts.SOON) &&
+    stockText?.includes(StockTexts.AVAILABLE)
+  );
+}
+
+function verifyInShopStock(stockText: string) {
+  return (
+    !stockText?.includes(StockTexts.CURRENTLY) &&
+    stockText?.includes(StockTexts.AVAILABLE)
+  );
 }
